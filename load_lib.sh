a99bbdec79073ba41cbe89c1d253a2ee16db2763
Stefano Mariani <stefano.mariani@unimore.it>
2024-09-10 10:23:47 +0200

    trying to debug lib.so CUDA

---
+++ load_lib.sh
@@ -0,0 +1,2 @@
+export LD_LIBRARY_PATH=/.env/lib/python3.10/site-packages/nvidia_cudnn_cu12-9.1.0.70-py3.10-linux-x86_64.egg/nvidia/cudnn/lib:${LD_LIBRARY_PATH}
+export LD_PRELOAD=/.env/lib/python3.10/site-packages/nvidia_cudnn_cu12-9.1.0.70-py3.10-linux-x86_64.egg/nvidia/cudnn/lib/libcudnn.so.9
