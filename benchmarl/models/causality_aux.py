b7d94965837866c903c842777d00fee2d78474fd
Stefano Mariani <stefano.mariani@unimore.it>
2024-09-11 10:21:30 +0200

    changing task, still not working

--- benchmarl/models/causality_aux.py
+++ benchmarl/models/causality_aux.py
@@ -14,6 +14,7 @@ class CausalActionsFilter:
         # TODO: better script path definition
         script_path = __file__.replace('causality_aux.py', 'causality_best')
         script_path = script_path.replace('/.env/lib/python3.10/site-packages/benchmarl-1.2.1-py3.10.egg', '')
+        #script_path = script_path.replace('/', '')
 
         self.device = kwargs.get('device', 'cuda' if torch.cuda.is_available() else 'cpu')
         self.path_best = f'{script_path}/{self.task}'
